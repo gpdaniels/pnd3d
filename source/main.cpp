92f3e41140030dd900a23eddd521aa3b4fc96854
Geoffrey Daniels <geoff@gpdaniels.com>
2024-03-25 22:17:47 +0000

    Add some missing physics code.

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,7 @@
 #include "octree_renderer.hpp"
 
 #include "brushes.hpp"
+#include "types/parallelepiped.hpp"
 #include "types/sprite.hpp"
 #include "utilities/gl.hpp"
 #include "utilities/timestamp.hpp"
@@ -36,6 +37,226 @@ static INT_PTR htex = 0;
 
 #define SURFPTR(oct,ind) (&((surf_t *)oct->sur.buf)[ind])
 
+static void orthofit (double *m, long c)
+{
+    double d, nm[9];
+    int i;
+
+        //(Cheap & simplified version of the 03/18/2006 algo)
+        //Note: this version assumes input matrix has positive determinant
+    for(;c>0;c--)
+    {
+        for(i=9-1;i>=0;i--) nm[i] = m[i];
+        m[0] += nm[4]*nm[8] - nm[5]*nm[7];
+        m[1] += nm[5]*nm[6] - nm[3]*nm[8];
+        m[2] += nm[3]*nm[7] - nm[4]*nm[6];
+        m[3] += nm[7]*nm[2] - nm[8]*nm[1];
+        m[4] += nm[8]*nm[0] - nm[6]*nm[2];
+        m[5] += nm[6]*nm[1] - nm[7]*nm[0];
+        m[6] += nm[1]*nm[5] - nm[2]*nm[4];
+        m[7] += nm[2]*nm[3] - nm[0]*nm[5];
+        m[8] += nm[0]*nm[4] - nm[1]*nm[3];
+        d = 1.0 / sqrt(m[0]*m[0] + m[1]*m[1] + m[2]*m[2]);
+        for(i=9-1;i>=0;i--) m[i] *= d;
+    }
+}
+
+static void spr_update_rdf (spr_t *spr)
+{
+    spr->r.x = spr->br.x*spr->ori[0] + spr->br.y*spr->ori[1] + spr->br.z*spr->ori[2];
+    spr->r.y = spr->br.x*spr->ori[3] + spr->br.y*spr->ori[4] + spr->br.z*spr->ori[5];
+    spr->r.z = spr->br.x*spr->ori[6] + spr->br.y*spr->ori[7] + spr->br.z*spr->ori[8];
+    spr->d.x = spr->bd.x*spr->ori[0] + spr->bd.y*spr->ori[1] + spr->bd.z*spr->ori[2];
+    spr->d.y = spr->bd.x*spr->ori[3] + spr->bd.y*spr->ori[4] + spr->bd.z*spr->ori[5];
+    spr->d.z = spr->bd.x*spr->ori[6] + spr->bd.y*spr->ori[7] + spr->bd.z*spr->ori[8];
+    spr->f.x = spr->bf.x*spr->ori[0] + spr->bf.y*spr->ori[1] + spr->bf.z*spr->ori[2];
+    spr->f.y = spr->bf.x*spr->ori[3] + spr->bf.y*spr->ori[4] + spr->bf.z*spr->ori[5];
+    spr->f.z = spr->bf.x*spr->ori[6] + spr->bf.y*spr->ori[7] + spr->bf.z*spr->ori[8];
+}
+
+//10/26/2011:optimized algo :)
+//WARNING:Assumes axis is unit length!
+static void axisrotate (point3d *p, point3d *ax, double w)
+{
+    point3d op;
+    double c, s, d;
+
+    c = cos(w); s = sin(w);
+    //P = cross(AX,P)*s + dot(AX,P)*(1-c)*AX + P*c;
+    op = (*p);
+    d = (op.x*ax->x + op.y*ax->y + op.z*ax->z)*(1.0-c);
+    p->x = (ax->y*op.z - ax->z*op.y)*s + ax->x*d + op.x*c;
+    p->y = (ax->z*op.x - ax->x*op.z)*s + ax->y*d + op.y*c;
+    p->z = (ax->x*op.y - ax->y*op.x)*s + ax->z*d + op.z*c;
+}
+
+static void collide_binsearch (oct_t *o0, point3d *op, point3d *or_, point3d *od, point3d *of,
+                                          point3d *np, point3d *nr, point3d *nd, point3d *nf,
+                               oct_t *o1, point3d *p1, point3d *r1, point3d *d1, point3d *f1, oct_hit_t *ohit)
+{
+    point3d hitp, hitr, hitd, hitf;
+    int k;
+
+        //Binary search..
+    for(k=8;k>0;k--)
+    {
+        hitp.x = (op->x + np->x)*.5;
+        hitp.y = (op->y + np->y)*.5;
+        hitp.z = (op->z + np->z)*.5;
+        hitr = *or_;
+        hitd = *od;
+        hitf = *of; //FIX:hack!
+
+        if (oct_touch_oct(o0,&hitp,&hitr,&hitd,&hitf,o1,p1,r1,d1,f1,ohit))
+              { *np = hitp; *nr = hitr; *nd = hitd; *nf = hitf; }
+        else { *op = hitp; *or_ = hitr; *od = hitd; *of = hitf; }
+    }
+}
+
+//Binary search collision..
+//  s0:     read: 1st object, starting posori, proposed delta movement
+//  s1:     read: 2nd (static) object to test collision with
+//  sfree: write: max posori not colliding
+//  shit:  write: min posori     colliding (= {0} + 1 lsd binsrch unit)
+//ohit: write: octree nodes, in order of {o0,o1}
+//Returns ratio travelled {0.0-1.0}
+static double collide_binsearch (spr_t *s0, spr_t *s1, spr_t *sfree, spr_t *shit, double dtim, oct_hit_t *ohit)
+{
+    spr_t tspr;
+    point3d uax;
+    double f, t, nt, tstep, mat[9], w;
+    int j, k;
+
+    w = s0->ax.x*s0->ax.x + s0->ax.y*s0->ax.y + s0->ax.z*s0->ax.z;
+    if (w != 0.0)
+    {
+        w = sqrt(w);
+        f = 1.0/w; uax.x = s0->ax.x*f; uax.y = s0->ax.y*f; uax.z = s0->ax.z*f;
+    }
+
+    t = 0.0; tstep = 1.0;
+    for(k=8;k>0;k--,tstep*=.5)
+    {
+        nt = t+tstep;
+        //---------------------------------------------------
+        tspr.p.x = s0->p.x + s0->v.x*nt*dtim;
+        tspr.p.y = s0->p.y + s0->v.y*nt*dtim;
+        tspr.p.z = s0->p.z + s0->v.z*nt*dtim;
+
+            //adjust pivot before rotation
+        tspr.p.x += (s0->r.x*s0->cen.x + s0->d.x*s0->cen.y + s0->f.x*s0->cen.z);
+        tspr.p.y += (s0->r.y*s0->cen.x + s0->d.y*s0->cen.y + s0->f.y*s0->cen.z);
+        tspr.p.z += (s0->r.z*s0->cen.x + s0->d.z*s0->cen.y + s0->f.z*s0->cen.z);
+
+        if (w != 0.0)
+        {
+            f = w*nt*dtim;
+            for(j=0;j<9;j++) mat[j] = s0->ori[(j/3)+(j%3)*3];
+            axisrotate((point3d *)&mat[0],&uax,f); //screw in
+            axisrotate((point3d *)&mat[3],&uax,f);
+            axisrotate((point3d *)&mat[6],&uax,f);
+            for(j=0;j<9;j++) tspr.ori[j] = mat[(j/3)+(j%3)*3];
+        }
+        else
+        {
+            memcpy(tspr.ori,s0->ori,sizeof(tspr.ori));
+        }
+        tspr.br = s0->br; tspr.bd = s0->bd; tspr.bf = s0->bf;
+        spr_update_rdf(&tspr);
+
+            //adjust pivot after rotation
+        tspr.p.x -= (tspr.r.x*s0->cen.x + tspr.d.x*s0->cen.y + tspr.f.x*s0->cen.z);
+        tspr.p.y -= (tspr.r.y*s0->cen.x + tspr.d.y*s0->cen.y + tspr.f.y*s0->cen.z);
+        tspr.p.z -= (tspr.r.z*s0->cen.x + tspr.d.z*s0->cen.y + tspr.f.z*s0->cen.z);
+        //---------------------------------------------------
+        j = oct_touch_oct(s0->oct,&tspr.p,&tspr.r,&tspr.d,&tspr.f,s1->oct,&s1->p,&s1->r,&s1->d,&s1->f,ohit);
+        if (!j) { (*sfree) = tspr; t = nt; if (t == 1.0) break; }
+            else { (*shit)  = tspr; }
+    }
+
+    if (t == 0.0)
+    {
+        (*sfree) = (*s0);
+    }
+    else
+    {
+        orthofit(sfree->ori,1); //not required, but doesn't hurt
+        spr_update_rdf(sfree);
+    }
+    return(t);
+}
+
+//see rigid1.pdf&rigid2.pdf for derivation
+    //---------------------------------------------------------
+    //e      r   Coefficient of restitution: 0=plastic, 1=elastic
+    //cp,cn  r   Contact point and unit vector nomal (provided by caller)
+    //?pos   r   Centroid of body, world space
+    //?ori   r   3x3 rotation matrix <rig,dow,for>, body to world space
+    //?vel   rw  Translational velocity, world space
+    //?rax   rw  Rotation axis scaled by angular velocity (rev/s), world space
+    //?rmas  r   Reciprocal of mass
+    //?rmoi  r   Inverse moment of inertia 3x3 matrix, ->BODY<- space.
+static void doimpulse3d (double e, point3d *cp, point3d *cn,
+    point3d *apos, double *aori, point3d *avel, point3d *arax, double armas, double *armoi,
+    point3d *bpos, double *bori, point3d *bvel, point3d *brax, double brmas, double *brmoi)
+{
+    point3d fp, padot, pbdot, ra, rb, ta, tb;
+    double fj, vrel, num, den, namoi[9], nbmoi[9];
+        //FIXFIXFIXFIX:remove!
+    printf("e:%g cp:<%g %g %g> cn:<%g %g %g>\n"
+             "apos:<%g %g %g> aori:<%g %g %g %g %g %g %g %g %g> armas:%g armoi:<%g %g %g %g %g %g %g %g %g>\n"
+             "bpos:<%g %g %g> bori:<%g %g %g %g %g %g %g %g %g> brmas:%g brmoi:<%g %g %g %g %g %g %g %g %g>\n",
+        e,cp->x,cp->y,cp->z,cn->x,cn->y,cn->z,
+        apos->x,apos->y,apos->z,aori[0],aori[1],aori[2],aori[3],aori[4],aori[5],aori[6],aori[7],aori[8],armas,armoi[0],armoi[1],armoi[2],armoi[3],armoi[4],armoi[5],armoi[6],armoi[7],armoi[8],
+        bpos->x,bpos->y,bpos->z,bori[0],bori[1],bori[2],bori[3],bori[4],bori[5],bori[6],bori[7],bori[8],brmas,brmoi[0],brmoi[1],brmoi[2],brmoi[3],brmoi[4],brmoi[5],brmoi[6],brmoi[7],brmoi[8]);
+    printf("avel:<%g %g %g> arax:<%g %g %g> bvel:<%g %g %g> brax:<%g %g %g>\n",avel->x,avel->y,avel->z,arax->x,arax->y,arax->z,bvel->x,bvel->y,bvel->z,brax->x,brax->y,brax->z);
+        //Calc moi in world coords
+        //I_world^-1 = R * I_body^-1 * R^T
+    simxform(namoi,aori,armoi);
+    simxform(nbmoi,bori,brmoi);
+        //Calc impulse magnitude (j)
+        //j = (-e-1) * (((Va + Wa x Rap) - (Vb + Wb x Rbp)) dot N) /
+        //   ( 1/ma + N dot ((Ia^-1 * (Rap cross N)) cross Rap)
+        //   + 1/mb + N dot ((Ib^-1 * (Rbp cross N)) cross Rbp))
+        //
+        //j = num/(armas + brmas + cn . ((a->Iinv * (ra x cn)) x ra)
+        //                         cn . ((b->Iinv * (rb x cn)) x rb));
+        //Calc relative velocity (vrel), parallel to contact normal (cn) at contact point (cp)
+    vecsub(&ra,cp,apos); //ra = cp - a->x;
+    vecsub(&rb,cp,bpos); //rb = cp - b->x;
+    veccross(&fp,arax,&ra); vecadd(&padot,avel,&fp); //padot = a->v + (a->w x ra)
+    veccross(&fp,brax,&rb); vecadd(&pbdot,bvel,&fp); //pbdot = b->v + (b->w x rb)
+    vecsub(&fp,&padot,&pbdot); vrel = vecdot(cn,&fp); //vrel = cn . (padot - pbdot);
+    num = (-e-1.0)*vrel;
+    den = armas+brmas;
+    veccross(&fp,&ra,cn); matvecmul(&ta,namoi,&fp); veccross(&fp,&ta,&ra); den += vecdot(cn,&fp);
+    veccross(&fp,&rb,cn); matvecmul(&tb,nbmoi,&fp); veccross(&fp,&tb,&rb); den += vecdot(cn,&fp);
+    fj = num/den;
+        //Do impulse:translation
+    vecscale(&fp,cn,fj*armas); vecadd(avel,avel,&fp); //nVa = Va + (j*N)/ma;
+    vecscale(&fp,cn,fj*brmas); vecsub(bvel,bvel,&fp); //nVb = Vb - (j*N)/mb;
+        //Do impulse:rotation
+    vecscale(&fp,&ta,fj); vecadd(arax,arax,&fp);       //nWa = Wa + Ia^-1 * (Rap cross (j*N));
+    vecscale(&fp,&tb,fj); vecsub(brax,brax,&fp);       //nWb = Wb - Ib^-1 * (Rbp cross (j*N));
+        //FIXFIXFIXFIX:remove!
+    printf("avel:<%g %g %g> arax:<%g %g %g> bvel:<%g %g %g> brax:<%g %g %g>\n",avel->x,avel->y,avel->z,arax->x,arax->y,arax->z,bvel->x,bvel->y,bvel->z,brax->x,brax->y,brax->z);
+}
+
+static void sprite_del (int k)
+{
+    int i, j;
+
+    sprnum--; spr[k] = spr[sprnum];
+    for(i=octnum-1;i>=0;i--)
+    {
+        for(j=sprnum-1;j>=0;j--) if (spr[j].oct == &oct[i]) break;
+        if (j >= 0) continue;
+        oct_free(&oct[i]);
+        octnum--; oct[i] = oct[octnum];
+        for(j=sprnum-1;j>=0;j--) if (spr[j].oct == &oct[octnum]) spr[j].oct = &oct[i];
+    }
+}
+
 int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hpinst, LPSTR cmdline, int ncmdshow);
 
 int main(int argc, char* argv[]) {
@@ -54,6 +275,7 @@ int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hpinst, LPSTR cmdline, int ncmdsho
     double d, tim = 0, otim, dtim, avgdtim = 0.0;
     float f;
     int isurf, obstatus = 0;
+    static oct_hit_t ohit[2];
 
     window_state window;
 
@@ -340,6 +562,82 @@ int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hpinst, LPSTR cmdline, int ncmdsho
             oct_drawline(window.xres, window.yres, window.xres-68,10,window.xres   ,10,0xffc0c0c0);
             avgdtim += (dtim-avgdtim)*.01;
             oct_drawtext6x8(window.xres, window.yres, window.xres-64,0,0xff00ffff,0xff0000ff,"%6.2f fps",1.0/avgdtim);
+
+            for(int i=sprnum-1;i>0;i--)
+            {
+                point3d lvec;
+                double dt, bdt, mat[9];
+                spr_t sfree, shit, bsfree, bshit;
+                oct_hit_t bohit[2];
+                int bj;
+
+                if (spr[i].cnt < 3) spr[i].v.y += dtim*.25; //gravity
+
+                oct_drawtext6x8(window.xres, window.yres, window.xres>>1,30+i*10,0xffffffff,0,"spr[%d].v: %f %f %f",i,spr[i].v.x,spr[i].v.y,spr[i].v.z); //FIXFIXFIXFIX:remove
+
+                bdt = 1.0;
+                for(int j=sprnum-1;j>=0;j--) ////FIX:doesn't handle hitting multiple sprites simultaneously properly :/
+                {
+                    if (i == j) continue;
+                    dt = collide_binsearch(&spr[i],&spr[j],&sfree,&shit,dtim,ohit);
+                    if (dt <= bdt) { bdt = dt; bj = j; bsfree = sfree; bshit = shit; bohit[0] = ohit[0]; bohit[1] = ohit[1]; }
+                }
+
+                    //precession (AngMom = Iworld*AngVel); see FIZEX3D.KC for derivation
+                simxform(mat,spr[i].ori,spr[i].moi); matvecmul(&lvec,mat,&spr[i].ax);
+
+                spr[i].p = bsfree.p; spr[i].r = bsfree.r; spr[i].d = bsfree.d; spr[i].f = bsfree.f;
+                memcpy(&spr[i].ori,&bsfree.ori,sizeof(spr[i].ori));
+                spr_update_rdf(&spr[i]);
+
+                    //precession (AngVel = Iworld^-1*AngMom); see FIZEX3D.KC for derivation
+                simxform(mat,spr[i].ori,spr[i].rmoi); matvecmul(&spr[i].ax,mat,&lvec);
+
+                if (bdt < 1.0)
+                {
+                    pgram3d_t bi;
+                    point3d dp0, dr0, dd0, df0, dp1, dr1, dd1, df1, dpos, hit, norm;
+
+                    //if (bdt == 0.0) { spr[i].v.x = 0; spr[i].v.y = 0.0; spr[i].v.z = 0.0; } //FIXFIXFIXFIX:prevents daffy ducking, but makes more stucks :/
+                    if (bdt == 0.0) spr[i].cnt++; else spr[i].cnt = 0;
+
+                    f = (1<<bohit[0].ls)*.5;
+                    dr0.x = bshit.r.x*f; dd0.x = bshit.d.x*f; df0.x = bshit.f.x*f;
+                    dr0.y = bshit.r.y*f; dd0.y = bshit.d.y*f; df0.y = bshit.f.y*f;
+                    dr0.z = bshit.r.z*f; dd0.z = bshit.d.z*f; df0.z = bshit.f.z*f;
+                    fp.x = bohit[0].x+f; fp.y = bohit[0].y+f; fp.z = bohit[0].z+f; oct_vox2worldpos(&bshit,&fp,&dp0);
+                    f = (1<<bohit[1].ls)*.5;
+                    dr1.x = spr[bj].r.x*f; dd1.x = spr[bj].d.x*f; df1.x = spr[bj].f.x*f;
+                    dr1.y = spr[bj].r.y*f; dd1.y = spr[bj].d.y*f; df1.y = spr[bj].f.y*f;
+                    dr1.z = spr[bj].r.z*f; dd1.z = spr[bj].d.z*f; df1.z = spr[bj].f.z*f;
+                    fp.x = bohit[1].x+f; fp.y = bohit[1].y+f; fp.z = bohit[1].z+f; oct_vox2worldpos(&spr[bj],&fp,&dp1);
+
+                    pgram3d_init(&bi,&dr0,&dd0,&df0,&dr1,&dd1,&df1);
+                    if (pgram3d_gethit(&bi,&dp0,&dp1,&dpos,&hit,&norm))
+                    {
+                        if (!bj) { spr[0].rmas = 0.0; for(int k=0;k<9;k++) spr[0].rmoi[k] = 0.0; } //Hack to prevent spr[0] from moving
+                        //if (keystatus[0x38]) { gravity = 0; fixhitit = 1; curspr = 1; hitp = bshit.p; hitr = bshit.r; hitd = bshit.d; hitf = bshit.f; break; } //FIX:debug only!
+
+                        oct_vox2worldpos(&spr[ i],&spr[ i].cen,&dp0);
+                        oct_vox2worldpos(&spr[bj],&spr[bj].cen,&dp1);
+                        doimpulse3d(0.5,&hit,&norm,&dp0,spr[ i].ori,&spr[ i].v,&spr[ i].ax,spr[ i].rmas,spr[ i].rmoi,
+                                                            &dp1,spr[bj].ori,&spr[bj].v,&spr[bj].ax,spr[bj].rmas,spr[bj].rmoi);
+                    }
+
+                    spr[i].tim += dtim;
+                    if (spr[i].tim < 1.0)
+                        { if (spr[i].tim >= 0.0) spr[i].imulcol = (spr[i].imulcol&0xffffff) + (std::min(std::max((int)(255.0-spr[i].tim*256.0),0),255)<<24); }
+                    else {
+                        sprite_del(i);
+                    }
+                }
+                else
+                {
+                    spr[i].cnt = 0;
+                    spr[i].tim = std::max(spr[i].tim-dtim*.5,-2.0);
+                }
+                if (spr[i].p.y > 4.0) { sprite_del(i); }
+            }
 
             oct_stopdraw(window);
         }
